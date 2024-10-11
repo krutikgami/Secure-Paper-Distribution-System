4ac9a07ff7c0d5c23428e4a917421e2b5f1342e3
Krutik Gami <krutikgami354@gmail.com>
2024-10-11 22:53:00 +0530

    index.js file bug fixed

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import connect_db from './db_connector.js';
 import userroutes from './routes/User.routes.js';
 import adminroutes from './routes/Admin.routes.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 dotenv.config({
     path: '../.env'
@@ -10,8 +12,17 @@ dotenv.config({
 
 const app = express();
 
+// Middleware to enable CORS
+app.use(cors({
+origin: process.env.CORS_ORIGIN,
+credentials: true
+}))
+
 // Middleware to parse JSON bodies
 app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({extended: true,limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());  
 
 // Debugging middleware to log request body
 app.use((req, res, next) => {
